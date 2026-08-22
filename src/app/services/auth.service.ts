d5de437e02a439ce449e5edb842bfde5ab4a1477
Tamiru Asbe <tamiruasbe10@gmail.com>
2026-08-22 12:39:03 +0300

    module-10/session-3

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -83,6 +83,10 @@ export interface LoginRequest {
   email: string;
   password: string;
 }
+export interface LoginResponse {
+  accessToken: string;
+  refreshToken: string;
+}
 
 // ==========================================
 // REGISTER RESPONSE
